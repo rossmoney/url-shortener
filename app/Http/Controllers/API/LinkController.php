acfe6b81cd701c05c686b446479755cc705b654f
Ross Money <5647850+rossmoney@users.noreply.github.com>
2024-12-28 17:02:16 +0000

    feat: Add Swagger Docs

--- app/Http/Controllers/API/LinkController.php
+++ app/Http/Controllers/API/LinkController.php
@@ -6,12 +6,34 @@ use App\Http\Controllers\Controller;
 use App\Models\Link;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(
+ *     title="URL Shortener",
+ *     version="1.0.0"
+ * )
+ */
 class LinkController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/encode",
+     *     summary="Encode a URL into a short form",
+     *     @OA\Parameter(
+     *         name="url",
+     *         in="query",
+     *         description="URL to shorten",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     tags={"Link"},
+     *     @OA\Response(response=200, description="Successful operation"),
+     *     @OA\Response(response=400, description="Invalid request")
+     * )
+     */
     public function encode(Request $request)
     {
         if (empty($request->input('url'))) {
-            abort(400);
+            return response()->json(['message' => 'Invalid request'], 400);
         }
 
         $originalUrl = $request->input('url');
@@ -20,17 +42,34 @@ class LinkController extends Controller
         return response()->json($link);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/decode",
+     *     summary="Decode a shortened URL into the original", 
+     *     @OA\Parameter(
+     *         name="url",
+     *         in="query",
+     *         description="Short URL (http://short.est/e58h6j)",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     tags={"Link"},
+     *     @OA\Response(response=200, description="Successful operation"),
+     *     @OA\Response(response=400, description="Invalid request"),
+     *     @OA\Response(response=404, description="URL Not Found!")
+     * )
+     */
     public function decode(Request $request)
     {
         if (empty($request->input('url'))) {
-            abort(400);
+            return response()->json(['message' => 'Invalid request'], 400);
         }
         
         $url = $request->input('url');
         $link = Link::fromShortened($url);
 
         if (empty($link) || empty($link->original_url)) {
-            abort(404);
+            return response()->json(['message' => 'URL Not Found!'], 404);
         }
 
         return response()->json($link);
@@ -38,6 +77,14 @@ class LinkController extends Controller
 
     /**
      * Display a listing of the resource.
+     */
+    /**
+     * @OA\Get(
+     *     path="/api/links",
+     *     summary="Return all stored links",
+     *     tags={"Link"},
+     *     @OA\Response(response=200, description="Successful operation")
+     * )
      */
     public function index()
     {
@@ -48,9 +95,28 @@ class LinkController extends Controller
     /**
      * Display the specified resource.
      */
+    /**
+     * @OA\Get(
+     *     path="/api/links/{id}",
+     *     summary="Return a single stored link by ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of URL to return",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     tags={"Link"},
+     *     @OA\Response(response=200, description="Successful operation"),
+     *     @OA\Response(response=404, description="ID not found")
+     * )
+     */
     public function show($id)
     {
         $link = Link::getById($id);
+        if (empty($link)) {
+            return response()->json(['message' => 'ID not found'], 404);
+        }
         return response()->json($link);
     }
 
